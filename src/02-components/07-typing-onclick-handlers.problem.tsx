4aeaf4a404c17cfd38e9d8609064aa3a274bbc8b
bora <nhatbaoxxd@gmail.com>
2024-02-20 18:45:06 +0700

    02-04. Typing Event Handlers

--- src/02-components/07-typing-onclick-handlers.problem.tsx
+++ src/02-components/07-typing-onclick-handlers.problem.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-interface ButtonProps {
+interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   className: string;
   children: React.ReactNode;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export const Button = ({ children, className, onClick }: ButtonProps) => {
@@ -10,5 +11,15 @@ export const Button = ({ children, className, onClick }: ButtonProps) => {
     <button onClick={onClick} className={className}>
       {children}
     </button>
+  );
+};
+
+const Parent = () => {
+  return (
+    <>
+      <Button onClick={() => {}} className="my-class">
+        Click me
+      </Button>
+    </>
   );
 };
