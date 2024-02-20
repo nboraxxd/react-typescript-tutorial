3817ff33a39aa383c47ed80f5b7d5c99f00cd2e7
bora <nhatbaoxxd@gmail.com>
2024-02-20 18:33:54 +0700

    02-02. Typing a Component as a Function

--- src/02-components/05-typing-components-as-functions.problem.tsx
+++ src/02-components/05-typing-components-as-functions.problem.tsx
@@ -4,11 +4,8 @@ interface Props {
   className: string;
 }
 
-/* @ts-expect-error */
-export const Button = (props: Props) => {
-  return {
-    ohDear: "123",
-  };
+export const Button: React.FC<Props> = (props) => {
+  return <button className={props.className}>Click me</button>;
 };
 
 const Parent = () => {
