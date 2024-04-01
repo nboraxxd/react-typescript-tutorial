445a6e15ebbaa540d3f325122ab19f09b1f95ce1
bora <nhatbaoxxd@gmail.com>
2024-04-01 21:03:42 +0700

    03-11_21

--- src/03-hooks/21-use-reducer.problem.ts
+++ src/03-hooks/21-use-reducer.problem.ts
@@ -1,28 +1,40 @@
-import { useReducer } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { Reducer, useReducer } from 'react'
+import { Equal, Expect } from '../helpers/type-utils'
 
-const reducer = (state: unknown, action: unknown) => {
+type ReducerState = { count: number }
+
+type ReducerAction =
+  | {
+      type: 'add'
+      add: number
+    }
+  | {
+      type: 'subtract'
+      subtract: number
+    }
+
+const reducer: Reducer<ReducerState, ReducerAction> = (state, action) => {
   switch (action.type) {
-    case "add":
-      return { count: state.count + action.add };
-    case "subtract":
-      return { count: state.count - action.subtract };
+    case 'add':
+      return { count: state.count + action.add }
+    case 'subtract':
+      return { count: state.count - action.subtract }
     default:
-      throw new Error();
+      throw new Error()
   }
-};
+}
 
-const [state, dispatch] = useReducer(reducer, { count: 0 });
+const [state, dispatch] = useReducer(reducer, { count: 0 })
 
-type tests = [Expect<Equal<typeof state.count, number>>];
+type tests = [Expect<Equal<typeof state.count, number>>]
 
-dispatch({ type: "add", add: 1 });
-
-// @ts-expect-error
-dispatch({ type: "SUBTRACT", subtract: 1 });
+dispatch({ type: 'add', add: 1 })
 
 // @ts-expect-error
-dispatch({ type: "add" });
+dispatch({ type: 'SUBTRACT', subtract: 1 })
 
 // @ts-expect-error
-dispatch({ type: "subtract", subtract: "123" });
+dispatch({ type: 'add' })
+
+// @ts-expect-error
+dispatch({ type: 'subtract', subtract: '123' })
